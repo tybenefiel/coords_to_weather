5d47fa0f6f9fc3e291e3c39c01196287dbe6910a
Ty Benefiel <tbenefiel2014@kellogg.northwestern.edu>
2013-11-10 11:04:27 -0600

    Finished
    I couldn't figure out how to make it only one page- I had to create two
    pages.  Because I based the long/lat on the address in the form, if
    there wasn't an address in the form, none of the logic worked and I
    would get an error.

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -2,18 +2,28 @@ require 'open-uri'
 require 'json'
 
 class CoordsController < ApplicationController
-  def fetch_weather
-    @latitude = 42.0538387
-    @longitude = -87.67721
-    your_api_key = ""
+    def fetch_weather
 
-    # Your code goes here.
-    # url = ?
-    # raw_data = ?
-    # parsed_data = ?
-    # @temperature = ?
-    # @minutely_summary = ?
-    # @hourly_summary = ?
-    # @daily_summary = ?
+    end
+
+  def local_weather
+    @address = params[:address]
+    @url_safe_address = URI.encode(@address)
+    @url ="http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}&sensor=true"
+
+    raw_data = open(@url).read
+    parsed_data =JSON.parse(raw_data)
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+
+
+    @weather_url ="https://api.forecast.io/forecast/d7bd4f49d7629dc9ddf45f2844c6a22e/#{@latitude},#{@longitude}"
+
+    weather_raw_data = open(@weather_url).read
+    weather_parsed_data =JSON.parse(weather_raw_data)
+   @temperature = weather_parsed_data["currently"]["temperature"]
+   @minutely_summary = weather_parsed_data["minutely"]["summary"]
+   @hourly_summary = weather_parsed_data["hourly"]["summary"]
+   @daily_summary = weather_parsed_data["daily"]["summary"]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 CoordsToWeather::Application.routes.draw do
   get("/weather", { :controller => "coords", :action => "fetch_weather" })
+  get("/local_weather", { :controller => "coords", :action => "local_weather" })
 end
